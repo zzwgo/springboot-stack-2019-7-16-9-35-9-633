d289fc8fe90961e7ddb57bf2c25824cf7c6956f0
zzw <kenzzw@163.com>
2019-07-16 23:40:29 +0800

    Add: add controller EmployeeController

---
+++ src/main/java/com/tw/apistackbase/controller/EmployeeController.java
@@ -0,0 +1,77 @@
+package com.tw.apistackbase.controller;
+
+import com.tw.apistackbase.model.Employee;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.tw.apistackbase.model.Employee.createTestEmployees;
+
+@RestController
+@RequestMapping("employees")
+public class EmployeeController {
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        return createTestEmployees();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getSpecEmployee(@PathVariable int id){
+        List<Employee> employees=createTestEmployees();
+        for(Employee employee:employees){
+            if(employee.getId()==id){
+                return employee;
+            }
+        }
+        return null;
+    }
+
+    @GetMapping(value = "", params = {"page","pageSize"})
+    public List<Employee> getPageEmployees (@RequestParam int page, @RequestParam int pageSize){
+        List<Employee> employees = createTestEmployees();
+        List<Employee> pageList=new ArrayList<>();
+        for (int i = (page - 1) * pageSize; i < page * pageSize; i++) {
+            if (i < employees.size()) {
+                pageList.add(employees.get(i));
+            }
+        }
+        return pageList;
+    }
+
+    @GetMapping(value = "", params = {"salary"})
+    public List<Employee> getSpeSalarycEmployee(@RequestParam(required = true) String salary){
+        return createTestEmployees().stream().filter(it->it.getGender().equals(salary)).collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public List<Employee> addEmployees(@RequestBody Employee employee){
+        List<Employee> employees=new ArrayList<>();
+        employees.add(employee);
+        return employees;
+    }
+    @PutMapping
+    public List<Employee> updateEmployee(@RequestBody Employee employee){
+        List<Employee> employees=createTestEmployees();
+        for(Employee currentEmployee:employees){
+            if(currentEmployee.getId()==employee.getId()){
+                employees.remove(currentEmployee);
+                employees.add(employee);
+                break;
+            }
+        }
+        return employees;
+    }
+    @DeleteMapping("/{id}")
+    public List<Employee> deleteEmployee(@PathVariable int id){
+        List<Employee> employees=createTestEmployees();
+        for(Employee currentEmployee:employees){
+            if(currentEmployee.getId()==id){
+                employees.remove(currentEmployee);
+                break;
+            }
+        }
+        return employees;
+    }
+}
